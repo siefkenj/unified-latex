6c84e6cf592dae8ab48b306e1f33493317e558a0
Jason Siefken <siefkenj@gmail.com>
2022-05-16 14:14:09 -0400

    Add lerna

--- scripts/package-consistency.ts
+++ scripts/package-consistency.ts
@@ -57,11 +57,19 @@ async function getImportsInDir(dirname): Promise<string[]> {
         );
 
         console.log(packageJsonFile);
-        
+
         const packageName = packageJson.name;
-        const computedPackageName = `@unified-latex/${path.basename(projectDir)}`
+        const computedPackageName = `@unified-latex/${path.basename(
+            projectDir
+        )}`;
         if (packageName !== computedPackageName) {
-            console.log(chalk.bgCyan.bold("Package-name/folder name mismatch"), "package is named", packageName, "but folder is named", path.basename(projectDir))
+            console.log(
+                chalk.bgCyan.bold("Package-name/folder name mismatch"),
+                "package is named",
+                packageName,
+                "but folder is named",
+                path.basename(projectDir)
+            );
         }
 
         if (missingImport.length > 0) {
@@ -77,91 +85,5 @@ async function getImportsInDir(dirname): Promise<string[]> {
             console.log(chalk.blue("     " + excessImport.join("\n     ")));
         }
 
-        // Figure out which files are imported
     }
-
-    //
-    //    console.log(chalk.bold("Processing"), filesToProcess.length, "files");
-    //    for (const file of filesToProcess) {
-    //        console.log(
-    //            "   ",
-    //            chalk.magenta(file.getDirectory().getBaseName()),
-    //            `/ ${file.getBaseName()}`
-    //        );
-    //
-    //        const outPath = path.join(file.getDirectoryPath(), "README.md");
-    //
-    //        const readme = generateReadme(file.getFilePath());
-    //
-    //        console.log(
-    //            chalk.gray("       Writing to", path.relative(__dirname, outPath))
-    //        );
-    //
-    //        await fs.writeFile(outPath, readme, { encoding: "utf-8" });
-    //    }
-    //
-    //    /*
-    //    const filename =
-    //        //   "./packages/unified-latex/unified-latex-types/index.ts";
-    //        "./packages/unified-latex/unified-latex-util-replace/index.ts";
-    //    //"./packages/unified-latex/unified-latex-util-trim/index.ts";
-    //    //"./packages/unified-latex/unified-latex-util-visit/index.ts";
-    //    */
-    //
-    //    function generateReadme(filename): string {
-    //        const sourceFile = project.getSourceFileOrThrow(filename);
-    //
-    //        const accumulatedExports = getDataOnAllExports(sourceFile);
-    //        const constantsMd = makeConstantsSection(accumulatedExports.constants);
-    //        const typesMd = makeTypesSection(accumulatedExports.types);
-    //        const funcsMd = makeFunctionsSection(accumulatedExports.funcs);
-    //        const pluginMd = makePluginsSection(accumulatedExports.plugins);
-    //
-    //        // Grab the comments from index.ts
-    //        const jsDoc = sourceFile.getLastToken().getFirstChild();
-    //        let introText = "";
-    //        if (Node.isJSDoc(jsDoc)) {
-    //            introText = jsDoc.getCommentText();
-    //        }
-    //
-    //        const directoryName = sourceFile.getDirectory().getBaseName();
-    //        let skeletonReadme = `# ${directoryName}`;
-    //        if (introText) {
-    //            skeletonReadme += "\n\n" + introText;
-    //        }
-    //        if (accumulatedExports.plugins.length > 0) {
-    //            skeletonReadme += "\n\n# Plugins";
-    //        }
-    //        if (accumulatedExports.funcs.length > 0) {
-    //            skeletonReadme += "\n\n# Functions";
-    //        }
-    //        if (accumulatedExports.constants.length > 0) {
-    //            skeletonReadme += "\n\n# Constants";
-    //        }
-    //        if (accumulatedExports.types.length > 0) {
-    //            skeletonReadme += "\n\n# Types";
-    //        }
-    //
-    //        // Assemble the dynamically-generated data
-    //        const target = gfmRemark.parse(skeletonReadme);
-    //
-    //        if (accumulatedExports.plugins.length > 0) {
-    //            inject("Plugins", target, pluginMd);
-    //        }
-    //        if (accumulatedExports.funcs.length > 0) {
-    //            inject("Functions", target, funcsMd);
-    //        }
-    //        if (accumulatedExports.constants.length > 0) {
-    //            inject("Constants", target, constantsMd);
-    //        }
-    //        if (accumulatedExports.types.length > 0) {
-    //            inject("Types", target, typesMd);
-    //        }
-    //
-    //        const renderedReadme = `${README_COMMENT}${gfmRemark.stringify(
-    //            target
-    //        )}`;
-    //
-    //        return renderedReadme;
-    //    }
 })();
