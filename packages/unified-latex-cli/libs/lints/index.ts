3117bc620be849c599af4c8e09efc0cda00ccd8f
Jason Siefken <siefkenj@gmail.com>
2022-05-23 21:11:47 -0400

    Add tests for cli

--- packages/unified-latex-cli/libs/lints/index.ts
+++ packages/unified-latex-cli/libs/lints/index.ts
@@ -1,4 +1,4 @@
-import { lints } from "@unified-latex/unified-latex-lint/dist";
+import { lints } from "@unified-latex/unified-latex-lint";
 
 export const availableLints = Object.fromEntries(
     Object.values(lints).map((lint) => [
