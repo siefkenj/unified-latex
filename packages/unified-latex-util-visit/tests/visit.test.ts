e872feb8b56e8e83ff84073d060c388543cf382b
Jason Siefken <siefkenj@gmail.com>
2023-04-25 17:22:46 -0400

    Test function should infer correctly with VisitInfo included

--- packages/unified-latex-util-visit/tests/visit.test.ts
+++ packages/unified-latex-util-visit/tests/visit.test.ts
@@ -1,8 +1,9 @@
 import { VFile } from "unified-lint-rule/lib";
 import util from "util";
 import { printRaw } from "@unified-latex/unified-latex-util-print-raw";
+import * as Ast from "@unified-latex/unified-latex-types";
 import { parse } from "../../unified-latex-util-parse";
-import { visit } from "../libs/visit";
+import { visit, VisitInfo } from "../libs/visit";
 
 /* eslint-env jest */
 
@@ -128,5 +129,50 @@ describe("unified-latex-util-visit", () => {
             }
         );
         expect(received).toEqual(expected);
+    });
+    it("issue #30 test function that accepts optional type should infer correctly", () => {
+        // This issue cannot be checked by Jest because it is an issue related to typescript
+        // types. It must be tested manually
+        let ast = parse("");
+        const testFunc1 = ((node: any) => false) as (
+            node: any
+        ) => node is Ast.Group;
+        const testFunc2 = ((node: any, info: any) => false) as (
+            node: any,
+            info: VisitInfo
+        ) => node is Ast.Group;
+        const testFunc3 = ((node: any, info: any, other?: any) => false) as (
+            node: any,
+            info: VisitInfo,
+            other?: any
+        ) => node is Ast.Group;
+
+        visit(
+            ast,
+            (nodes) => {
+                // CHECK: inferred type of `node` should be `Ast.Group`
+            },
+            {
+                test: testFunc1,
+            }
+        );
+        visit(
+            ast,
+            (nodes) => {
+                // CHECK: inferred type of `node` should be `Ast.Group`
+            },
+            {
+                test: testFunc2,
+            }
+        );
+        visit(
+            ast,
+            (nodes) => {
+                // CHECK: inferred type of `node` should be `Ast.Group`
+            },
+            {
+                test: testFunc3,
+            }
+        );
     });
 });
