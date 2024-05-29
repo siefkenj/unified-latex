ac2333409bb3975996df4647b0800bbe3033adb5
Nahlee Naria Khan <renee.khan@mail.utoronto.ca>
2024-05-29 13:54:32 -0400

    Revert "added some test cases"
    This reverts commit 26c09acf6f7c68ea4250562e34f9fda1e5c8008f.

--- packages/unified-latex-to-pretext/tests/break-on-boundaries-test.ts
+++ packages/unified-latex-to-pretext/tests/break-on-boundaries-test.ts
@@ -11,53 +11,5 @@ console.log = (...args) => {
 };
 
 describe("unified-latex-to-pretext:break-on-boundaries", () => {
-    let value: string;
-
-    it("can break on sections", () => {
-        value = String.raw`\section{Foo}
-                            Hi, this is a section
-                                
-                            \section{Bar}
-                            This is another section`;
-
-        const parser = getParser();
-        const ast = parser.parse(value);
-
-        // break-on-boundaries work done here
-
-        expect(printRaw(ast)).toEqual([String.raw`\begin{_section}
-                                                \title{Foo}
-                                                Hi, this is a section
-                                            \end{_section}
-                                            \begin{_section}
-                                                \title{Bar}
-                                                This is another section
-                                            \end{_section}`]);
-    });
-    it("can break on a subsection", () => {
-        value = String.raw`\subsection{subsection}
-                            Hi, this is a subsection`;
-
-        const parser = getParser();
-        const ast = parser.parse(value);
-
-        // break-on-boundaries work done here
-
-        expect(printRaw(ast)).toEqual([String.raw``]);
-    });
-    it("can break on subsection between subsections properly", () => {
-        value = String.raw`\section{First Section}
-                            Hi, this is a section
-                            \subsection{Inner Subsection}
-                            This is a subsection
-                            \section{Second Subsection}
-                            This is another section`;
-
-        const parser = getParser();
-        const ast = parser.parse(value);
-
-        // break-on-boundaries work done here
-
-        expect(printRaw(ast)).toEqual([String.raw``]);
-    });
+   
 });
