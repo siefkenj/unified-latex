292de9f6492818f6dc4f0ca25de6466442973da5
Nahlee Naria Khan <renee.khan@mail.utoronto.ca>
2024-05-29 14:41:37 -0400

    fixed test file

--- packages/unified-latex-to-pretext/tests/break-on-boundaries-test.ts
+++ packages/unified-latex-to-pretext/tests/break-on-boundaries-test.ts
@@ -1,7 +1,6 @@
 import { describe, it, expect } from "vitest";
 import util from "util";
 import { getParser } from "@unified-latex/unified-latex-util-parse";
-import { report_macros, expand_user_macros } from "../libs/report-and-expand-macros";
 import { printRaw } from "@unified-latex/unified-latex-util-print-raw";
 
 // Make console.log pretty-print by default
@@ -12,4 +11,53 @@ console.log = (...args) => {
 
 describe("unified-latex-to-pretext:break-on-boundaries", () => {
    
+    let value: string;
+
+    it("can break on sections", () => {
+        value = String.raw`\section{Foo}
+                            Hi, this is a section
+                                
+                            \section{Bar}
+                            This is another section`;
+
+        const parser = getParser();
+        const ast = parser.parse(value);
+
+        // break-on-boundaries work done here
+
+        expect(printRaw(ast)).toEqual([String.raw`\begin{_section}
+                                                \title{Foo}
+                                                Hi, this is a section
+                                            \end{_section}
+                                            \begin{_section}
+                                                \title{Bar}
+                                                This is another section
+                                            \end{_section}`]);
+    });
+    it("can break on a subsection", () => {
+        value = String.raw`\subsection{subsection}
+                            Hi, this is a subsection`;
+
+        const parser = getParser();
+        const ast = parser.parse(value);
+
+        // break-on-boundaries work done here
+
+        expect(printRaw(ast)).toEqual([String.raw``]);
+    });
+    it("can break on subsection between subsections properly", () => {
+        value = String.raw`\section{First Section}
+                            Hi, this is a section
+                            \subsection{Inner Subsection}
+                            This is a subsection
+                            \section{Second Subsection}
+                            This is another section`;
+
+        const parser = getParser();
+        const ast = parser.parse(value);
+
+        // break-on-boundaries work done here
+
+        expect(printRaw(ast)).toEqual([String.raw``]);
+    });
 });
