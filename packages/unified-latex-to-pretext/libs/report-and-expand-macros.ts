30bac19646dfa9b22d45d1390eb1ed6f0bb33588
Nahlee Naria Khan <renee.khan@mail.utoronto.ca>
2024-05-29 13:55:34 -0400

    Revert "updated code"
    This reverts commit 2e5fdbd1c1f0e612a300fe8ae0be02b9ff854275.

--- packages/unified-latex-to-pretext/libs/report-and-expand-macros.ts
+++ packages/unified-latex-to-pretext/libs/report-and-expand-macros.ts
@@ -1,8 +1,9 @@
 import * as Ast from "@unified-latex/unified-latex-types";
-import { newcommandMatcher, newcommandMacroToSubstitutionAst } from "@unified-latex/unified-latex-util-macros";
+import { newcommandMatcher,listNewcommands, newcommandMacroToSubstitutionAst, expandMacrosExcludingDefinitions, expandMacros } from "@unified-latex/unified-latex-util-macros";
 import { anyMacro, match } from "@unified-latex/unified-latex-util-match";
 import { visit } from "@unified-latex/unified-latex-util-visit";
-import { KATEX_SUPPORT } from "./pre-conversion-subs/katex-subs";
+import { attachNeededRenderInfo, katexSpecificMacroReplacements, KATEX_SUPPORT } from "./pre-conversion-subs/katex-subs";
+import { attachMacroArgs } from "@unified-latex/unified-latex-util-arguments";
 import { replaceNode } from "@unified-latex/unified-latex-util-replace";
 
 /**
@@ -10,18 +11,22 @@ import { replaceNode } from "@unified-latex/unified-latex-util-replace";
  */
 export function report_macros(ast: Ast.Ast): string[] {
     let unsupported: string[] = [];
+    //attachNeededRenderInfo(ast);
 
     // match a macro supported by Katex
-    const isSupported = match.createMacroMatcher(KATEX_SUPPORT.macros);
+    // const isKatexMacro = match.createMacroMatcher(katexSpecificMacroReplacements);
 
     // visit all nodes
     visit(ast, (node, info) => {
         // macro in math mode
         if (anyMacro(node) && info.context.hasMathModeAncestor) {
             const macro_name = node.content;
+            //console.log(node);
+            //console.log(katexSpecificMacroReplacements[node.content]);
 
             // check if not supported by katex
-            if (!isSupported(node)) {
+            // if (!isKatexMacro(node)) {
+            if (!KATEX_SUPPORT.macros.includes(macro_name)) {
                 unsupported.push(macro_name);
             }
         }
@@ -33,7 +38,7 @@ export function report_macros(ast: Ast.Ast): string[] {
 /**
  * Expands user-defined macros
  */
-export function expand_user_macros(ast: Ast.Ast): void {
+export function expand_user_macros(ast: Ast.Ast): void  {
     replaceNode(ast, (node) => {
         if (anyMacro(node) && newcommandMatcher(node)) {
             return newcommandMacroToSubstitutionAst(node);
